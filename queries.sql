729340389471718a2f55c6a28dd1ebc4ed906fb2
Viral <vaidyaviral@gmail.com>
2021-07-15 13:19:02 -0500

    Final commit Assignment 4

--- queries.sql
+++ queries.sql
@@ -1,7 +1,30 @@
 ## Part 1: Test it with SQL
+1)  At this point, you will have one table, job. In queries.sql under “Part 1”, list the columns and their data types in the table.
 
+id - int
+employer - varchar
+name - varchar
+skills - varchar
+
+
+SELECT * FROM techjobs.job;
+------------------------------------------------------------------------
 ## Part 2: Test it with SQL
+##SQL TASK: In queries.sql under “Part 2”, write a query to list the names of the employers in St. Louis City.
 
+SELECT * FROM techjobs.employer  where location = 'St. Louis'
+-------------------------------------------------------------------------
 ## Part 3: Test it with SQL
-
+## “Part 3”, write the SQL statement to remove the job table.
+Drop table techjobs.job;
+-------------------------------------------------------------------------
 ## Part 4: Test it with SQL
+
+##write a query to return a list of the names and descriptions of all skills that are attached to jobs in alphabetical order.
+If a skill does not have a job listed, it should not be included in the results of this query.
+
+SELECT skill.name, skill.description
+FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+ORDER BY name ASC;
+-------------------------------------------------------------------------
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,40 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location is required")
+    @Size(message = "Location must be between 5 and 100 characters.", min = 5, max = 100)
+    private String location;
+
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
+
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 
 }
